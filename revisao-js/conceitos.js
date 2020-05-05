1b0532e68c4739fd7a00e470d8e313d6c57757b9
laisrhianne <lays.rhianne@gmail.com>
2020-05-05 14:58:21 -0300

    conceitos js

---
+++ revisao-js/conceitos.js
@@ -0,0 +1,6 @@
+/*
+* Arrow functions possuem this estático
+* Em Arrow functions o this depende de onde a função foi declarada
+* Em funções normais o this varia a depender de como a função for chamada
+* Arrays são objetos
+*/
