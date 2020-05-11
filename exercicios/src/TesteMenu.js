f4e53c19047c786c45d589da1381e5ad8337ef05
laisrhianne <lays.rhianne@gmail.com>
2020-05-11 19:34:49 -0300

    testes

--- exercicios/src/TesteMenu.js
+++ exercicios/src/TesteMenu.js
@@ -1,16 +1,29 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+
+
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <Button>Enviar MQTT</Button>
     </View>
   );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
